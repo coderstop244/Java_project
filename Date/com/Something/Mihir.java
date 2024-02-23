a5260241ee0721491ce2d7ff9ea8a95db18ffdde
coderstop244 <152056461+coderstop244@users.noreply.github.com>
2024-02-23 03:48:38 -0500

    Add files via upload

---
+++ Date/com/Something/Mihir.java
@@ -0,0 +1,15 @@
+package com.Something;
+
+public class Mihir {
+    public static void main(String [] args){
+        private String name;
+        public Mihir(String name) {
+            this.name= name;
+        }
+     public String GetNme() {
+        return name;
+     }  
+    }
+    
+    
+}
